25217a701b3a3ca70b43968eeaf621a892508e30
YakoViTo <kavirjosegermanav@gmail.com>
2023-10-10 16:10:51 -0400

    create tasks

--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,5 +1,39 @@
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from 'react-router-dom';
+
 export function TasksFormPage() {
-    return (
-      <div>Tasks Form Page</div>
-    )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async data => {
+    await createTask(data);
+    navigate("/tasks");
+  });
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>title is required</span>}
+
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        {errors.description && <span>desciption is required</span>}
+
+        <button>save</button>
+      </form>
+    </div>
+  );
 }
